7c006a19b02ade6bc771144d587361ac5db2e384
nazmulhossen1 <nazmulhossen539@gmail.com>
2023-11-06 01:07:55 +0600

    get data to server site and somting

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 
 const express = require('express')
+const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
-const port = 5001
+const port = process.env.PORT || 5001;
 require('dotenv').config();
+
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const BlogDB = "my_blog_database_name";
 
 /* DB URI */
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.idkvt6k.mongodb.net/?retryWrites=true&w=majority`;
@@ -22,6 +30,26 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+
+    const blogCollection = client.db(BlogDB).collection("blogs");
+
+
+    // phones single data
+    app.post('/api/v1/blogs', async (req, res) => {
+      const blog = req.body;
+      const result = await blogCollection.insertOne(blog);
+      console.log(result);
+      res.send(result);
+    });
+
+    app.get('/api/v1/blogs', async (req, res) => {
+      const result = await blogCollection.find().toArray();
+      res.send(result);
+    });
+
+
+
+
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
@@ -40,9 +68,7 @@ run().catch(console.dir);
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-app.get('/api/v1/allblogs',(req, res) => {
-  res.send('konodatainai')
-})
+
 
 app.listen(port, () => {
   console.log(`The blogopolish server running ${port}`)
